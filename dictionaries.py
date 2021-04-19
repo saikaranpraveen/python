407b6cf03d967f15c350062e1b60a1bcb087e13e
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 16:03:07 +0530

    Creating a dictionary and looping through it

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+person = {
+    "Name" : "Cruise",
+    "Movie" : "Mission Impossible",
+    "Age" : 50
+}
+
+for key, value in person.items():
+    print(key + ":",value)           #looping through dictionaries
