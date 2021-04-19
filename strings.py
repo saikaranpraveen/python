652d3699e71367e1bac1f28191674bfc17cbab32
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 15:20:00 +0530

    Strings and their manipulation

---
+++ strings.py
@@ -0,0 +1,22 @@
+my_string = "I have a black car."
+
+#print the string
+print(my_string)
+
+#indexing the string
+print(my_string[1])
+
+#finding out string length
+print(len(my_string))
+
+#looping through a string
+for a in my_string:
+    print(a)
+
+#check if an entity is present in a string
+print("black" in my_string)
+
+#slicing strings
+print(my_string[2:5])
+
+print(my_string[-7:-1])
