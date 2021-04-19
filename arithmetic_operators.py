6f25d3cadbb95afdc5e2a0bbb9093dadea3d7c48
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 15:16:40 +0530

    Basic arithmetic operations

---
+++ arithmetic_operators.py
@@ -0,0 +1,26 @@
+x = 200
+y = 20
+
+#addition
+z = x + y
+print (str(x) + " plus " + str(y) + " is " + str(z))
+
+#subtraction
+z = x - y
+print (str(x) + " minus " + str(y) + " is " + str(z))
+
+#multiplication
+z = x * y
+print (str(x) + " times " + str(y) + " is " + str(z))
+
+#division
+z = x / y
+print (str(x) + " divided by " + str(y) + " is " + str(z))
+
+#modulus
+z = x % y
+print ("The remainder when " + str(x) + " is divided by " + str(y) + " is " + str(z) )
+
+#exponential
+z = x ** y
+print (str(x) + " to the times " + str(y) + " is " + str(z))
