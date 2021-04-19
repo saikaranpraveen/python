d12025d9129479c36a8378abfd2fd5ce7fd91cea
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-19 15:16:06 +0530

    Variables and Data Types

---
+++ variables.py
@@ -0,0 +1,18 @@
+name = "Daniel"
+age = 25
+weight = 80.5
+hobbies = ["Running", "Swimming", "Guitar"]
+movies = ("Speed", "Titanic", "Avengers")
+vehicles = {
+    "brand" : "Honda",
+    "moder" : "City",
+}
+is_adult = True
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(hobbies))
+print(type(movies))
+print(type(vehicles))
+print(type(is_adult))
